10d789d77e5259c3b192faa8d22135adacc069ec
Jon Capa <jonathan.capa@gmail.com>
2016-03-06 23:49:02 -0500

    Starting to add german translations but toggled off

---
+++ site/js/r6-maps.lang-terms.de.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var R6MapLangTermsGerman = (function(R6MapLangTerms, undefined) {
+  var terms = {
+    mapNames: {
+      bank: 'BANK GERMAN'
+    }
+  };
+
+  R6MapLangTerms.loadLangPack(terms);
+})(R6MapLangTerms);
