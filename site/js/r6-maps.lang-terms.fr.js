a21e4a5198dafd3e76763fef1e8de3d06209d2b6
Jon Capa <jonathan.capa@gmail.com>
2016-03-13 20:18:40 -0400

    Initial French translations added

---
+++ site/js/r6-maps.lang-terms.fr.js
@@ -0,0 +1,107 @@
+'use strict';
+
+var R6MapsLangTermsFrench = (function(R6MapsLangTerms, undefined) {
+  var terms = {
+    general: {
+      pageTitle: 'Carte de siège - {mapName}',
+      cameraViewCaption: '{floorName} de caméra',
+      shortcutTip: 'Raccourci: 1 {shortcut}'
+    },
+    floorNames: {
+      basement: 'SS',
+      firstFloor: 'RDC',
+      secondFloor: '1',
+      thirdFloor: '2',
+      fourthFloor: '3',
+      roof: 'Toit'
+    },
+    objectives: {
+      bombShortA: 'A',
+      bombShortB: 'B',
+      bomb: 'Bombe',
+      hostageShort: 'O',
+      hostage: 'Orage',
+      secureShort: 'S',
+      secure: 'Sécurisation',
+      showAll: 'Montre tout'
+    },
+    legend: {
+      breakableWalls: 'Murs cassables',
+      breakableFloorTraps: 'Trappes de plancher cassables',
+      ceilingHatches: 'Trappes de plafond',
+      lineOfSightWalls: 'Ligne de murs à vue',
+      lineOfSightFloors: 'Ligne d\'étages à vue',
+      droneTunnels: 'Tunnels drone',
+      objectives: 'Objectifs',
+      insertionPoints: 'Points d\'insertion',
+      securityCameras: 'Des caméras de sécurité',
+      skylights: 'Puits de lumière',
+      onFloorAboveOrBelow: 'A l\'étage au-dessus ou au-dessous'
+    },
+    mapNames: {
+      kanal: 'Canal'
+    },
+    mapRooms: {
+      kanal: {
+        boatGarage: 'Hangar à bateaux',
+        boatSupplies: 'Remise du hangar',
+        pipes: 'Tuyaux percés',
+        boatSuppliesHallway: 'Couloir<br/>remise<br/>du hangar',
+        lockerRoom: 'Vestiares',
+        coastGuardStairs: 'Escalier<br/>garde-côte',
+        showers: 'Douches',
+        loadingDock: 'Quai de chargement',
+        machineHallway: 'Salle des<br/>machines',
+        controlCenterStairs: 'Escalier centre<br/>de contrôle',
+        holdingRoom: 'Cellule de détention',
+        holdingRoomHallway: 'Couloir<br/>cellule<br/>de détention',
+        radio: 'Radio',
+        coastGuardOffice: 'Bureau<br/>garde-côte',
+        archives: 'Archives',
+        coastGuardHall: 'Couloir<br/>garde-côte<br/>',
+        mainEntrance: 'Entrée<br/>principale',
+        lounge: 'Lounge',
+        bridge: 'Pont',
+        modelRoom: 'Salle de la maquette',
+        securityRoom: 'Salle de<br/>sécurité',
+        projectorRoom: 'Salle de projection',
+        mapsOffice: 'Salle des cartes',
+        mapsOfficeHallway: 'Couloir salle des cartes',
+        kitchen: 'Cuisine',
+        cafeteria: 'Caféteéria',
+        plantsHallway: 'Couloir avec plantes',
+        thirdFloorExit: 'Sortie du<br/>3d étage<br/>',
+        controlRoom: 'Salle de contrôle',
+        electricRoom: 'Local<br/>électrique',
+        controlRoomHallway: 'Couloir salle de contrôle',
+        serverRoom: 'Salle des serveurs',
+        lockgate: 'Écluse',
+        quayContainers: 'Conteneurs à quai',
+        lockgateTunnel: 'Tunnel<br/>à écluse',
+        waterWalkway: 'Passerelle',
+        quayConstruction: 'Quai en contruction',
+        constructionSite: 'Chantier<br/>',
+        constructionEntrance: 'Entrée<br/>du chantier',
+        parkingAlley: 'Allée du parking',
+        parkingEntrance: 'Entrée<br/>du parking',
+        middleRoad: 'Route<br/>du milieu',
+        forkliftAlley: 'Allée char.<br/>Élévateurs',
+        frontLawn: 'Pelouse de devant',
+        basementStairs: 'Escalier du sous-sol',
+        coastGuardRoof: 'Toit<br/>de la<br/>garde-côte',
+        roofBrickPile: 'Tas<br/>de<br/>tuiles',
+        balcony: 'Balcon',
+        controlCenterRoof: 'Toit<br/>du centre<br/>de contrôle',
+        dockStairs: 'Escalier du quai',
+        parking: 'Parking',
+        boatCrane: 'Grue'
+      }
+    }
+  };
+
+  //R6MapsLangTerms.loadLangPack(terms);
+
+  return  {
+    terms: terms
+  };
+})(R6MapsLangTerms);
