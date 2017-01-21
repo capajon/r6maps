84e38e22943f3afa9d8abfca3039b3ba7e04f205
Jon Capa <jon.capa@D2L.com>
2017-01-20 19:33:51 -0500

    Using conventional jquery var name pattern

--- dev/js/r6-maps.map-render.js
+++ dev/js/r6-maps.map-render.js
@@ -328,7 +328,7 @@ var R6MapsRender = (function($,window,document,R6MapsLangTerms,undefined) {
     return html;
   };
 
-  var renderMap = function renderMap(mapData, mapElements, svgMapWrapper, getResetDimensions) {
+  var renderMap = function renderMap(mapData, $mapElements, $svgMapWrappers, $mapPanelLabels, getResetDimensions) {
     var html = '',
       resetDimensions = getResetDimensions(),
       spinnerMarginLeft = resetDimensions.centerLeft,
@@ -347,27 +347,27 @@ var R6MapsRender = (function($,window,document,R6MapsLangTerms,undefined) {
     html += getSpawnPointsHtml(mapData.spawnPoints);
     html += getLegendHtml();
 
-    mapElements.html(html);
-    $('.map-panel-label').html(getPanelLabelsHtml(mapData.floors));
-    svgMapWrapper.html(getCamerasLosHtml(mapData.cameras));
+    $mapElements.html(html);
+    $mapPanelLabels.html(getPanelLabelsHtml(mapData.floors));
+    $svgMapWrappers.html(getCamerasLosHtml(mapData.cameras));
   };
 
-  var setEnableScreenshots = function setEnableScreenshots(mapWrappers, isEnabled) {
+  var setEnableScreenshots = function setEnableScreenshots($mapWrappers, isEnabled) {
     if (isEnabled) {
-      mapWrappers.removeClass('disable-cameras');
+      $mapWrappers.removeClass('disable-cameras');
     } else {
-      mapWrappers.addClass('disable-cameras');
+      $mapWrappers.addClass('disable-cameras');
     }
   };
 
-  var setRoomLabelStyle = function setRoomLabelStyle(mapElements, style) {
+  var setRoomLabelStyle = function setRoomLabelStyle($mapElements, style) {
     ROOM_LABEL_STYLES.forEach(function(roomLabelStyle) {
-      mapElements.removeClass(ROOM_LABEL_CSS_TEXT[roomLabelStyle]);
+      $mapElements.removeClass(ROOM_LABEL_CSS_TEXT[roomLabelStyle]);
     });
-    mapElements.addClass(ROOM_LABEL_CSS_TEXT[style]);
+    $mapElements.addClass(ROOM_LABEL_CSS_TEXT[style]);
   };
 
-  var setupMapPanels = function setupMapPanels(mapPanelWrapper, numPanels) {
+  var setupMapPanels = function setupMapPanels($mapPanelWrappers, numPanels) {
     var html;
 
     for (var x = 0; x < numPanels; x++) {
@@ -384,19 +384,19 @@ var R6MapsRender = (function($,window,document,R6MapsLangTerms,undefined) {
       html += '</div>'; // end center-helper
       html += '</div>'; // end map-main
       html += '</div>'; // end map-wrapper
-      mapPanelWrapper.append(html);
+      $mapPanelWrappers.append(html);
     }
   };
 
   var showFloor = function showFloor(
     selectedFloorIndex,
-    mapPanelWrapper,
-    mapWrappers,
+    $mapPanelWrappers,
+    $mapWrappers,
     minFloorIndex,
     maxFloorIndex
   ) {
-    mapPanelWrapper.attr('selected-floor-index', selectedFloorIndex);
-    var numPanels = mapPanelWrapper.attr('map-panel-count');
+    $mapPanelWrappers.attr('selected-floor-index', selectedFloorIndex);
+    var numPanels = $mapPanelWrappers.attr('map-panel-count');
 
     if (numPanels > 2)  {
       selectedFloorIndex = Math.max(minFloorIndex, selectedFloorIndex - 1);
@@ -404,19 +404,17 @@ var R6MapsRender = (function($,window,document,R6MapsLangTerms,undefined) {
     var tempMinIndex = Math.max(minFloorIndex, maxFloorIndex - numPanels + 1),
       startingIndex = Math.min(tempMinIndex, selectedFloorIndex);
 
-    mapWrappers.each(function(index, map) {
-      var mapWrapper = $(map);
-
-      $(mapWrapper).attr('show-floor-index', Math.min(startingIndex, maxFloorIndex));
+    $mapWrappers.each(function(index, map) {
+      $(map).attr('show-floor-index', Math.min(startingIndex, maxFloorIndex));
       startingIndex++;
     });
   };
 
-  var showObjective = function showObjective(objective, mapElements) {
+  var showObjective = function showObjective(objective, $mapElements) {
     var objectivePrefix = 'show-objective-';
 
-    mapElements.removeClassPrefix(objectivePrefix);
-    mapElements.addClass(objectivePrefix + objective);
+    $mapElements.removeClassPrefix(objectivePrefix);
+    $mapElements.addClass(objectivePrefix + objective);
   };
 
   return  {
